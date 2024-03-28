ed3dc87d78c89cd5cf218ae49a2c02e6755481d4
bigTogu <silvia.kenaan@gmail.com>
2024-03-28 20:30:23 +0100

    refactor:  IAM roles and policies, define Lambda functions and API Gateway endpoints

--- Infraestructure/Terraform/main.tf
+++ Infraestructure/Terraform/main.tf
@@ -23,33 +23,43 @@ provider "aws" {
 
 
 # Define IAM roles and policies
-resource "aws_iam_role" "iam_policy_for_lambda" {
-  name         = "aws_iam_policy_for_terraform_aws_lambda_role"
-  path         = "/"
-  description  = "AWS IAM Policy for managing aws lambda role"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-      ],
-    "Resource": "arn:aws:logs:*:*:*",
-    "Effect": "Allow"
-    }
-  ]
-}
-EOF
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "lambda.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
 }
 
-resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
-  role        = aws_iam_role.lambda_role.name
-  policy_arn  = aws_iam_policy.iam_policy_for_lambda.arn
+resource "aws_iam_policy" "iam_policy_for_lambda" {
+  name        = "aws_iam_policy_for_terraform_aws_lambda_role"
+  description = "AWS IAM Policy for managing AWS Lambda role"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Resource = "arn:aws:logs:*:*:*",
+        Effect   = "Allow"
+      }
+    ]
+  })
 }
+
+resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_lambda_role" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.iam_policy_for_lambda.arn
+}
+
 
 # Define resources
 resource "aws_dynamodb_table" "TaskTable" {
@@ -92,42 +102,44 @@ resource "aws_s3_bucket" "TaskStorage" {
 # Define Lamda ZIP files
 data "archive_file" "zip_create_task_lamda" {
   type        = "zip"
-  source_dir  = "${path.module}/Infraestructure/lambda/create_task.py"
-  output_path = "${path.module}/Infraestructure/create_task.zip"
+  source_dir  = "${path.module}/../lambda"
+  output_path = "${path.module}/lambda_functions.zip"
 }
 
-data "archive_file" "zip_list_task_lamda" {
-  type        = "zip"
-  source_dir  = "${path.module}/Infraestructure/lambda/list_task.py"
-  output_path = "${path.module}/Infraestructure/list_task.zip"
-}
 
 # Define Lambda functions
 resource "aws_lambda_function" "createScheduledTask" {
-  filename         = "${path.module}/Infraestructure/create_task.zip"
+  filename         = "${path.module}/lambda_functions.zip"
   function_name    = "createScheduledTask"
   role             = aws_iam_role.lambda_role.arn
   handler          = "create_task.lambda_handler"
   runtime          = "python3.8"
-  depends_on       = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
+  depends_on       = [aws_iam_role_policy_attachment.attach_iam_policy_to_lambda_role]
 }
 
 resource "aws_lambda_function" "listScheduledTask" {
-  filename         = "${path.module}/Infraestructure/list_task.zip"
+  filename         = "${path.module}/lambda_functions.zip"
   function_name    = "listScheduledTask"
   role             = aws_iam_role.lambda_role.arn
   handler          = "list_task.lambda_handler"
   runtime          = "python3.8"
-  depends_on       = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
+  depends_on       = [aws_iam_role_policy_attachment.attach_iam_policy_to_lambda_role]
 }
 
+resource "aws_lambda_function" "executeScheduledTask" {
+  filename         = "${path.module}/lambda_functions.zip"
+  function_name    = "executeScheduledTask"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "task_storage.lambda_handler"
+  runtime          = "python3.8"
+  depends_on       = [aws_iam_role_policy_attachment.attach_iam_policy_to_lambda_role]
+}
 
 # Define AWS API Gateway
 resource "aws_api_gateway_rest_api" "TaskAPI" {
   name        = "TaskAPI"
   description = "This is an API for the Task API"
 }
-
 
 # Define endpoints
 resource "aws_api_gateway_resource" "CreateTask" {
@@ -174,6 +186,36 @@ resource "aws_api_gateway_integration" "ListTaskIntegration" {
   resource_id = aws_api_gateway_resource.ListTask.id
   http_method = aws_api_gateway_method.ListTaskMethod.http_method
   type = "AWS_PROXY"
-  integration_http_method = "GET"
+  integration_http_method = "POST"
   uri = aws_lambda_function.listScheduledTask.invoke_arn
+}
+
+# Deploy API Gateway
+resource "aws_api_gateway_deployment" "TaskAPIDeployment" {
+  depends_on = [aws_api_gateway_integration.CreateTaskIntegration, aws_api_gateway_integration.ListTaskIntegration]
+  rest_api_id = aws_api_gateway_rest_api.TaskAPI.id
+  stage_name = "unused-stage"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Deploy API Gateway Stage
+resource "aws_api_gateway_stage" "TaskAPIStage" {
+  stage_name = "dev"
+  rest_api_id = aws_api_gateway_rest_api.TaskAPI.id
+  deployment_id = aws_api_gateway_deployment.TaskAPIDeployment.id
+}
+
+# Define cloudwatch event brigde for executeScheduledTask
+resource "aws_cloudwatch_event_rule" "TaskEventRule" {
+  name        = "TaskEventRule"
+  description = "Rule for executeScheduledTask"
+  schedule_expression = "rate(1 minute)"
+}
+# define event target for executeScheduledTask
+resource "aws_cloudwatch_event_target" "TaskEventTarget" {
+  rule = aws_cloudwatch_event_rule.TaskEventRule.name
+  arn  = aws_lambda_function.executeScheduledTask.arn
 }
