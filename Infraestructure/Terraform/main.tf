75eae01672d2391be6ef0367e89760ac84fe9b8a
bigTogu <silvia.kenaan@gmail.com>
2024-03-28 18:38:08 +0100

    feat: Add IAM roles and policies, define Lambda functions and API methods

--- Infraestructure/Terraform/main.tf
+++ Infraestructure/Terraform/main.tf
@@ -21,6 +21,36 @@ provider "aws" {
 # Add here all the infraestructure logic
 
 
+
+# Define IAM roles and policies
+resource "aws_iam_role" "iam_policy_for_lambda" {
+  name         = "aws_iam_policy_for_terraform_aws_lambda_role"
+  path         = "/"
+  description  = "AWS IAM Policy for managing aws lambda role"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+    "Resource": "arn:aws:logs:*:*:*",
+    "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
+  role        = aws_iam_role.lambda_role.name
+  policy_arn  = aws_iam_policy.iam_policy_for_lambda.arn
+}
+
 # Define resources
 resource "aws_dynamodb_table" "TaskTable" {
   name           = "TaskTable"
@@ -60,41 +90,35 @@ resource "aws_s3_bucket" "TaskStorage" {
 
 
 # Define Lamda ZIP files
-data "archive_file" "lambda_functions" {
+data "archive_file" "zip_create_task_lamda" {
   type        = "zip"
-  source_dir  = "${path.module}/Infraestructure/lambda"
-  output_path = "${path.module}/Infraestructure/lambda_function_payload.zip"
+  source_dir  = "${path.module}/Infraestructure/lambda/create_task.py"
+  output_path = "${path.module}/Infraestructure/create_task.zip"
+}
+
+data "archive_file" "zip_list_task_lamda" {
+  type        = "zip"
+  source_dir  = "${path.module}/Infraestructure/lambda/list_task.py"
+  output_path = "${path.module}/Infraestructure/list_task.zip"
 }
 
 # Define Lambda functions
-resource "aws_lambda_function" "CreateTask" {
-  function_name    = "CreateTask"
+resource "aws_lambda_function" "createScheduledTask" {
+  filename         = "${path.module}/Infraestructure/create_task.zip"
+  function_name    = "createScheduledTask"
+  role             = aws_iam_role.lambda_role.arn
   handler          = "create_task.lambda_handler"
   runtime          = "python3.8"
-  filename         = data.archive_file.lambda_functions.output_path
-  source_code_hash = data.archive_file.lambda_functions.output_base64sha256
-  role             = "arn:aws:iam::123456789012:role/lambda-role"
-
-
-  tags = {
-    Name        = "CreateTask"
-    Environment = "Development"
-  }
+  depends_on       = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
 }
 
-resource "aws_lambda_function" "ListTask" {
-  function_name    = "ListTask"
+resource "aws_lambda_function" "listScheduledTask" {
+  filename         = "${path.module}/Infraestructure/list_task.zip"
+  function_name    = "listScheduledTask"
+  role             = aws_iam_role.lambda_role.arn
   handler          = "list_task.lambda_handler"
   runtime          = "python3.8"
-  filename         = data.archive_file.lambda_functions.output_path
-  source_code_hash = data.archive_file.lambda_functions.output_base64sha256
-  role             = "arn:aws:iam::123456789012:role/lambda-role"
-
-
-  tags = {
-    Name        = "ListTask"
-    Environment = "Development"
-  }
+  depends_on       = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
 }
 
 
@@ -119,3 +143,37 @@ resource "aws_api_gateway_resource" "ListTask" {
 }
 
 
+# Define API methods
+resource "aws_api_gateway_method" "CreateTaskMethod" {
+  rest_api_id = aws_api_gateway_rest_api.TaskAPI.id
+  resource_id = aws_api_gateway_resource.CreateTask.id
+  http_method = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method" "ListTaskMethod" {
+  rest_api_id = aws_api_gateway_rest_api.TaskAPI.id
+  resource_id = aws_api_gateway_resource.ListTask.id
+  http_method = "GET"
+  authorization = "NONE"
+}
+
+
+# Define API integrations
+resource "aws_api_gateway_integration" "CreateTaskIntegration" {
+  rest_api_id = aws_api_gateway_rest_api.TaskAPI.id
+  resource_id = aws_api_gateway_resource.CreateTask.id
+  http_method = aws_api_gateway_method.CreateTaskMethod.http_method
+  type = "AWS_PROXY"
+  integration_http_method = "POST"
+  uri = aws_lambda_function.createScheduledTask.invoke_arn
+}
+
+resource "aws_api_gateway_integration" "ListTaskIntegration" {
+  rest_api_id = aws_api_gateway_rest_api.TaskAPI.id
+  resource_id = aws_api_gateway_resource.ListTask.id
+  http_method = aws_api_gateway_method.ListTaskMethod.http_method
+  type = "AWS_PROXY"
+  integration_http_method = "GET"
+  uri = aws_lambda_function.listScheduledTask.invoke_arn
+}
