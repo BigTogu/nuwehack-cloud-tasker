1d8a8ecbe12f96547292437ad5ca519386fc68a5
bigTogu <silvia.kenaan@gmail.com>
2024-03-28 17:54:00 +0100

    feat: Add infrastructure logic and define resources, Lambda functions, and API endpoints

--- Infraestructure/Terraform/main.tf
+++ Infraestructure/Terraform/main.tf
@@ -19,3 +19,103 @@ provider "aws" {
 }
 
 # Add here all the infraestructure logic
+
+
+# Define resources
+resource "aws_dynamodb_table" "TaskTable" {
+  name           = "TaskTable"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "task_id"
+  attribute {
+    name = "task_id"
+    type = "S"
+  }
+
+  # Encriptado de datos para proteger la confidencialidad de los datos almacenados
+  server_side_encryption {
+    enabled = true
+  }
+
+  # Permite restaurar la tabla a un estado anterior en caso de pérdida de datos, etc.
+  point_in_time_recovery {
+    enabled = true
+  }
+
+  tags = {
+    Name        = "TaskTable"
+    Environment = "Development"
+  }
+
+}
+
+resource "aws_s3_bucket" "TaskStorage" {
+  bucket = "taskstorage"
+
+  tags = {
+    Name        = "TaskStorage"
+    Environment = "Development"
+  }
+
+}
+
+
+# Define Lamda ZIP files
+data "archive_file" "lambda_functions" {
+  type        = "zip"
+  source_dir  = "${path.module}/Infraestructure/lambda"
+  output_path = "${path.module}/Infraestructure/lambda_function_payload.zip"
+}
+
+# Define Lambda functions
+resource "aws_lambda_function" "CreateTask" {
+  function_name    = "CreateTask"
+  handler          = "create_task.lambda_handler"
+  runtime          = "python3.8"
+  filename         = data.archive_file.lambda_functions.output_path
+  source_code_hash = data.archive_file.lambda_functions.output_base64sha256
+  role             = "arn:aws:iam::123456789012:role/lambda-role"
+
+
+  tags = {
+    Name        = "CreateTask"
+    Environment = "Development"
+  }
+}
+
+resource "aws_lambda_function" "ListTask" {
+  function_name    = "ListTask"
+  handler          = "list_task.lambda_handler"
+  runtime          = "python3.8"
+  filename         = data.archive_file.lambda_functions.output_path
+  source_code_hash = data.archive_file.lambda_functions.output_base64sha256
+  role             = "arn:aws:iam::123456789012:role/lambda-role"
+
+
+  tags = {
+    Name        = "ListTask"
+    Environment = "Development"
+  }
+}
+
+
+# Define AWS API Gateway
+resource "aws_api_gateway_rest_api" "TaskAPI" {
+  name        = "TaskAPI"
+  description = "This is an API for the Task API"
+}
+
+
+# Define endpoints
+resource "aws_api_gateway_resource" "CreateTask" {
+  rest_api_id = aws_api_gateway_rest_api.TaskAPI.id
+  parent_id   = aws_api_gateway_rest_api.TaskAPI.root_resource_id
+  path_part   = "createtask"
+}
+
+resource "aws_api_gateway_resource" "ListTask" {
+  rest_api_id = aws_api_gateway_rest_api.TaskAPI.id
+  parent_id   = aws_api_gateway_rest_api.TaskAPI.root_resource_id
+  path_part   = "listtask"
+}
+
+
